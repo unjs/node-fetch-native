cc53eb7541d5d9c673efd6a2b01ed9c57ea2085d
Pooya Parsa <pyapar@gmail.com>
2022-05-12 00:18:43 +0200

    fix(types): add missing type of `AbortController`

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,10 @@
-declare const fetch: typeof globalThis.fetch
-declare const Blob: typeof globalThis.Blob
-declare const File: typeof globalThis.File
-declare const FormData: typeof globalThis.FormData
-declare const Headers: typeof globalThis.Headers
-declare const Request: typeof globalThis.Request
-declare const Response: typeof globalThis.Response
+export declare const fetch: typeof globalThis.fetch
+export declare const Blob: typeof globalThis.Blob
+export declare const File: typeof globalThis.File
+export declare const FormData: typeof globalThis.FormData
+export declare const Headers: typeof globalThis.Headers
+export declare const Request: typeof globalThis.Request
+export declare const Response: typeof globalThis.Response
+export declare const AbortController: typeof globalThis.AbortController
 
-export { fetch, Blob, File, FormData, Headers, Request, Response }
 export default fetch
