fe77e7a02c5778e87d487b8a37e0d15c61ef10f7
Pooya Parsa <pooya@pi0.io>
2022-09-20 22:27:51 +0200

    fix: add hotfix for stackblitz support (#10)

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,18 @@ import _fetch, {
 
 import _AbortController from 'abort-controller'
 
+// Hotfix for https://github.com/unjs/node-fetch-native/issues/10
+const isStackblitz = process.env.SHELL === '/bin/jsh'
+if (isStackblitz && !globalThis.fetch) {
+  // @ts-ignore
+  globalThis.fetch = (url: any, opts: any) => {
+    if (typeof url === 'string' && url.includes('//[::]')) {
+      url = url.replace('//[::]', '//0.0.0.0')
+    }
+    return fetch(url, opts)
+  }
+}
+
 export const fetch = globalThis.fetch || _fetch
 export default fetch
 
