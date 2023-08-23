f46566f79ac5f31e53bd0f0f6386640c0a92de6e
Pooya Parsa <pooya@pi0.io>
2023-08-23 04:49:14 +0200

    fix: use soft warning for node bundle warn

--- src/node.ts
+++ src/node.ts
@@ -23,8 +23,8 @@ function checkNodeEnvironment() {
     !globalThis.process?.versions?.node &&
     !globalThis.process?.env.DISABLE_NODE_FETCH_NATIVE_WARN
   ) {
-    throw new Error(
-      "Node.js compatible build of `node-fetch-native` is being used in a non-Node.js environment. Please make sure you are using proper export conditions or report this issue to https://github.com/unjs/node-fetch-native. You can set `process.env.DISABLE_NODE_FETCH_NATIVE_WARN` to disable this warning.",
+    console.warn(
+      "[node-fetch-native] Node.js compatible build of `node-fetch-native` is being used in a non-Node.js environment. Please make sure you are using proper export conditions or report this issue to https://github.com/unjs/node-fetch-native. You can set `process.env.DISABLE_NODE_FETCH_NATIVE_WARN` to disable this warning.",
     );
   }
 }
